67676e050234334af44395ddceaf852db56bb5a1
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-02-27 14:35:12 +0530

    Update rt.tf

--- rt.tf
+++ rt.tf
@@ -12,16 +12,11 @@ resource "aws_route_table" "route" {
   }
 }
 
-#route 1
 
-resource "aws_route_table_association" "route1" {
+
+#association
+
+resource "aws_route_table_association" "route-as" {
   subnet_id      = aws_subnet.public-subnet1.id
-  route_table_id = aws_route_table.route.id
-}
-
-#route 2
-
-resource "aws_route_table_association" "route2" {
-  subnet_id      = aws_subnet.public-subnet2.id
   route_table_id = aws_route_table.route.id
 }
