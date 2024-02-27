b1725bc0260e0a0919fa78ce32f70819bf7d0526
SADA SIVA REDDY <145244419+Sada-Siva-Reddy07@users.noreply.github.com>
2024-02-27 23:49:07 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -7,6 +7,6 @@ sudo chmod 666 /var/run/docker.sock
 sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/chintuu77/wordpress.git 
+git clone https://github.com/Sada-Siva-Reddy07/wordpress.git 
 cd wordpress
 docker-compose up -d
