dfc6584052e8ac6d3349ba4e8b3fda6b27e72a8b
SADA SIVA REDDY <145244419+Sada-Siva-Reddy07@users.noreply.github.com>
2024-02-27 23:54:29 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,10 +1,10 @@
 #public-subnet1 creation
 
 resource "aws_subnet" "public-subnet1" {
-  vpc_id                  = aws_vpc.vivek.id
+  vpc_id                  = aws_vpc.sada.id
   cidr_block              = var.subnet1_cidr
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-north-1b"
+  availability_zone       = "us-west-1b"
 
   tags = {
     Name = "public-subnet1"
