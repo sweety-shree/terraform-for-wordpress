a10837d2c30025b623184d0cdfeeeb7ed8974755
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-02-27 14:29:30 +0530

    Update data.sh

--- data1.sh
+++ data1.sh
@@ -8,4 +8,5 @@ sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/chintuu77/wordpress.git 
+cd wordpress
 docker-compose up -d
