170ba78d91c6994b5d9954b56829293b455293ec
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-02-27 12:56:48 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "public-subnet1" {
   vpc_id                  = aws_vpc.vivek.id
   cidr_block              = var.subnet1_cidr
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-2a"
+  availability_zone       = "eu-north-1b"
 
   tags = {
     Name = "public-subnet1"
@@ -17,7 +17,7 @@ resource "aws_subnet" "public-subnet2" {
   vpc_id                  = aws_vpc.vivek.id
   cidr_block              = var.subnet2_cidr
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-2b"
+  availability_zone       = "eu-north-1a"
 
   tags = {
     Name = "public-subnet2"
@@ -30,7 +30,7 @@ resource "aws_subnet" "private-subnet1" {
   vpc_id                  = aws_vpc.vivek.id
   cidr_block              = var.subnet3_cidr
   map_public_ip_on_launch = false
-  availability_zone       = "us-east-2a"
+  availability_zone       = "eu-north-1b"
 
   tags = {
     Name = "private-subnet1"
@@ -43,7 +43,7 @@ resource "aws_subnet" "private-subnet2" {
   vpc_id                  = aws_vpc.vivek.id
   cidr_block              = var.subnet4_cidr
   map_public_ip_on_launch = false
-  availability_zone       = "us-east-2b"
+  availability_zone       = "eu-north-1a"
 
   tags = {
     Name = "private-subnet2"
@@ -56,7 +56,7 @@ resource "aws_subnet" "private-subnet3" {
   vpc_id                  = aws_vpc.vivek.id
   cidr_block              = var.subnet5_cidr
   map_public_ip_on_launch = false
-  availability_zone       = "us-east-2a"
+  availability_zone       = "eu-north-1b"
   tags = {
     Name = "private-subnet3"
   }
@@ -68,7 +68,7 @@ resource "aws_subnet" "private-subnet4" {
   vpc_id                  = aws_vpc.vivek.id
   cidr_block              = var.subnet6_cidr
   map_public_ip_on_launch = false
-  availability_zone       = "us-east-2b"
+  availability_zone       = "eu-north-1a"
   tags = {
     Name = "private-subnet4"
   }
