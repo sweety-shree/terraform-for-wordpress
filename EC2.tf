9e660bada4db6d2e9e79335a4bda23e8d8b3e137
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-02-27 12:29:25 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ecomm" {
   ami                         = "ami-0c7f9161f8491665f"
   instance_type               = "t2.micro"
-  key_name                    = "WP"
+  key_name                    = "JN"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
@@ -14,7 +14,7 @@ resource "aws_instance" "ecomm" {
 resource "aws_instance" "food" {
   ami                         = "ami-0c7f9161f8491665f"
   instance_type               = "t2.micro"
-  key_name                    = "WP"
+  key_name                    = "JN"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet2.id
   associate_public_ip_address = true
