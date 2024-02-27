9494e90dcfc848b3590dbaf15340b1614df61154
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-02-27 14:26:46 +0530

    Update websg.tf

--- websg.tf
+++ websg.tf
@@ -9,13 +9,6 @@ resource "aws_security_group" "web-sg" {
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
