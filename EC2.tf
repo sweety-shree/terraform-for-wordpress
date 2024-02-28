532c8f2ed4649e976b5c665d8daa9696ff46a09e
SADA SIVA REDDY <145244419+Sada-Siva-Reddy07@users.noreply.github.com>
2024-02-28 10:29:22 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "wordpress8" {
-  ami                         = "ami-02cc6142066a5587f"
+  ami                         = "ami-07761f3ae34c4478d"
   instance_type               = "t2.micro"
-  key_name                    = "jenkins"
+  key_name                    = "WORDPRESS"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
