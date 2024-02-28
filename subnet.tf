ebd0d7b5c82a0f23836ec7df38729abaf2fd7bd3
SADA SIVA REDDY <145244419+Sada-Siva-Reddy07@users.noreply.github.com>
2024-02-28 10:32:48 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "public-subnet1" {
   vpc_id                  = aws_vpc.sada.id
   cidr_block              = var.subnet1_cidr
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-west-1b"
+  availability_zone       = "us-east-1b"
 
   tags = {
     Name = "public-subnet1"
