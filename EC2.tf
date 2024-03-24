8c2cf1385614cb9731414af24c6cb09ca580efb4
sweety-shree <152768615+sweety-shree@users.noreply.github.com>
2024-03-24 11:56:11 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,8 +1,8 @@
 #instance
 resource "aws_instance" "wordpress8" {
-  ami                         = "ami-0830c9faf0efc29ff"
+  ami                         = "ami-026255a2746f88074"
   instance_type               = "t2.micro"
-  key_name                    = "california"
+  key_name                    = "Mumbai"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
